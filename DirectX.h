762bfe6f853c3ae6b2fd52662bc9091f7c6fb1e4
青木智滉 <@MSI>
2023-06-14 16:40:37 +0900

    テクスチャの貼り付け

--- DirectX.h
+++ DirectX.h
@@ -4,7 +4,7 @@
 #include <dxgi1_6.h>
 #include <cassert>
 #include <dxgidebug.h>
-#include <DirectXTex.h>
+#include "externals/DirectXTex/DirectXTex.h"
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
 #pragma comment(lib,"dxguid.lib")
